c255dee567fba6f97d14667b8a0436b8ab80906c
William Lacey <William.Lacey@gel.com>
2023-08-31 16:52:50 -0400

    Initial Commit

---
+++ Scripts/Character.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+var speed = 400
+@onready var body: CharacterBody2D = $"."
+var pushForce = 1000
+func getInput():
+	var dir = Input.get_vector("left","right","up","down")
+	velocity = dir * speed
+	
+func _physics_process(delta):
+	getInput()
+	
+	if body.move_and_slide(): # true if collided
+		for i in body.get_slide_collision_count():
+			var col = body.get_slide_collision(i)
+			if col.get_collider() is RigidBody2D:
+				col.get_collider().apply_force(col.get_normal() * -pushForce)
